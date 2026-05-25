d40e00faf3ae120beb68ecdb704945ed630c4d53
aayam <aayamjain04@gmail.com>
2026-05-25 23:51:20 +0530

    shape 1

---
+++ Sheet 2/shape1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main(){
+    int n;
+    std::cin>>n;
+    //study again n again
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=(n+1-i);j++){
+            std::cout<<"*";
+        }
+        std::cout<<std::endl;
+    }
+    return 0;
+}
